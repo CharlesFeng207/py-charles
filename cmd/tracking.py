8f78f41199fdcb7a833e937fab4bbc5f1a554b37
Unknown <CharlesFeng207@gmail.com>
2018-03-27 00:25:03 +0800

    s

--- cmd/tracking.py
+++ cmd/tracking.py
@@ -12,7 +12,7 @@ from openpyxl.utils import get_column_letter
 def find_tracking_row_objs():
 
     src_data_start_row = int(json_data["src_data_start_row"])
-    src_id_row = json_data["src_id_row"]
+  
     time_range_cols = json_data["time_range_cols"].split(",")
 
     column_letters = map(lambda i: get_column_letter(i), range(1, sheet_src.max_column + 1))
@@ -56,26 +56,8 @@ def process_working_table():
         "%Y-%m-%d", time_start), time.strftime("%Y-%m-%d", time_end))
 
     shutil.copy(json_data["temp_working"], working_output_path)
-    workbook_working = load_workbook(working_output_path)
-    sheet_working = workbook_working.active
 
-    writing_row_index = int(json_data["temp_working_id_row"])
-
-    column_letters = map(lambda i: get_column_letter(i), range(1, sheet_working.max_column + 1))
-    column_letters_id_dic = { k:sheet_working["{}{}".format(k, writing_row_index)].value for k in column_letters}
-
-    temp_working_data_start_row = int(json_data["temp_working_data_start_row"])
-    for obj in selected_cell_objs:
-        for letter in column_letters_id_dic.keys():
-            cell_id = column_letters_id_dic[letter]
-            if cell_id in obj:
-                sheet_working["{}{}".format(letter, temp_working_data_start_row)].value = obj[cell_id]
-           
-        temp_working_data_start_row += 1
-    
-    workbook_working.save(working_output_path)
-
-    print "{} created!".format(working_output_path)
+    make_table(working_output_path, int(json_data["temp_working_id_row"]), int(json_data["temp_working_data_start_row"]), src_selected_objs)
 
 def process_delay_table():
     
@@ -83,7 +65,51 @@ def process_delay_table():
         "%Y-%m-%d", time_start), time.strftime("%Y-%m-%d", time_end))
 
     shutil.copy(json_data["temp_delay"], delay_output_path)
-    sheet_delay = load_workbook(delay_output_path).active
+
+    delay_combine_number_id = json_data["delay_combine_number_id"]
+    delay_combine_id = sheet_src["{}{}".format(json_data["delay_combine_col"], src_id_row)].value
+
+    delay_obj_dic = {}
+
+    for obj in src_selected_objs:
+        if delay_combine_id not in obj:
+            continue
+
+        delay_combine_id_value = obj[delay_combine_id]
+        if delay_combine_id_value not in delay_obj_dic:
+            delay_obj_dic[delay_combine_id_value] = {}
+        
+        key_list_obj = delay_obj_dic[delay_combine_id_value]
+
+        for k in obj:
+            if k not in key_list_obj:
+                key_list_obj[k] = []
+            if obj[k] not in key_list_obj[k]:
+                key_list_obj[k].append(obj[k])
+    
+    delay_objs = map(lambda x:{k : ','.join(map(str, x[k])) for k in x}, delay_obj_dic.values())
+
+    make_table(delay_output_path, int(json_data["delay_working_id_row"]), int(json_data["delay_working_data_start_row"]), delay_objs)
+
+def make_table(output_path, id_row, writing_row, target_objs):
+    
+    workbook_target = load_workbook(output_path)
+    sheet_target = workbook_target.active
+
+    column_letters = map(lambda i: get_column_letter(i), range(1, sheet_target.max_column + 1))
+    column_letters_id_dic = { k:sheet_target["{}{}".format(k, id_row)].value for k in column_letters}
+
+    for obj in target_objs:
+        for letter in column_letters_id_dic.keys():
+            cell_id = column_letters_id_dic[letter]
+            if cell_id in obj:
+                sheet_target["{}{}".format(letter, writing_row)].value = obj[cell_id]
+           
+        writing_row += 1
+    
+    workbook_target.save(output_path)
+
+    print "{} created!".format(output_path)
 
 if len(sys.argv) == 3:
     json_path = sys.argv[1]
@@ -106,16 +132,19 @@ if len(sys.argv) == 3:
 
         workbook_src = load_workbook(json_data["src"])
         sheet_src = workbook_src[json_data["src_sheet"]]
+        src_id_row = json_data["src_id_row"]
 
         print "load complete! wait for processing ..."
 
         # find tracking rows by time info and save as dictionary
-        selected_cell_objs = find_tracking_row_objs()
+        src_selected_objs = find_tracking_row_objs()
 
         print "from {} to {}, there are {} rows selected!".format(time_start_str, time_end_str, len(selected_cell_objs))
 
+        # make working table
         process_working_table()
 
+        # make delay table
         process_delay_table()
 
 else:
